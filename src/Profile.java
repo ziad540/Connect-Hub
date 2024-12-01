c262a54e3322ce2748f751941612b615b4d0a782
Nour_Azab <nourazab123y@gmail.com>
2024-12-02 00:40:08 +0200

    prototype for profile gui

---
+++ src/Profile.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Profile {
+    private JPanel profileWindow;
+    private JLabel coverPhotoLabel;
+    private JLabel profilePhotoLabel;
+    private JList list1;
+    private JTable table1;
+    private JLabel bioLabel;
+    private JButton viewProfileButton;
+
+    public Profile() {
+        viewProfileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
