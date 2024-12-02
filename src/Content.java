0578b4727a8f7ff7804926b7b3d39bed95ef24fc
JOekh1122 <joekh04@gmail.com>
2024-12-03 01:18:05 +0200

    Create Post and Story

---
+++ src/Content.java
@@ -0,0 +1,45 @@
+import java.time.LocalDateTime;
+
+public abstract class Content {
+    private String contentId;
+    private String autherId;
+    private String content;
+    private String imagePath;
+    LocalDateTime timestamp = LocalDateTime.now();
+
+    public Content(String contentId,String autherId,String content,String imagePath){
+       this.content=contentId;
+       this.autherId=autherId;
+       this.content=content;
+       this.imagePath=imagePath;
+       this.timestamp = LocalDateTime.now();
+    }
+
+    public String getContentId() {
+        return contentId;
+    }
+
+    public String getAutherId() {
+        return autherId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Content ID: " + contentId + ", Author ID: " + autherId + ", Timestamp: " + timestamp;
+    }
+}
+
+
