3e2105bf555a18bc5d1e3635dfd910a3349857d2
ziad emad <eziad8002@gmail.com>
2024-12-02 23:16:55 +0200

    reset password
    rest password form but not ready yet

--- src/ProfileDetails.java
+++ src/ProfileDetails.java
@@ -34,7 +34,7 @@ public class ProfileDetails extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose();
-                p.setVisible(true);
+//                p.setVisible(true);
             }
         });
         editCoverPhotoButton.addActionListener(new ActionListener() {
