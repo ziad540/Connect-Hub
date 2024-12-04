bb97fa92e58735f6f939f3373093f6c14182c1f2
ziad emad <eziad8002@gmail.com>
2024-12-04 17:45:42 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+// add to gui
+        new NewsFeedgui();
+//        new FreindGui();
+//        new Addpost();
 
-// add to gui 
-      //new NewsFeedgui();
-        //new FreindGui();
-        //new PostAdd();
+        //  Profile profile =new Profile();
+        usersdatabase user =new usersdatabase("src/users.json");
+        user.load();
 
-       // Profile profile =new Profile();
+        postdatabase post= new postdatabase();
+        post.load();
+
+        STORIESDATABASE ST=new STORIESDATABASE();
+        ST.load();
 
 
     }
