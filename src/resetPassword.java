4227be8af56da29f7126e4ac796dc7d6badeff9a
Nour_Azab <nourazab123y@gmail.com>
2024-12-05 23:04:27 +0200

    updated edit windows to always stay on top

--- src/resetPassword.java
+++ src/resetPassword.java
@@ -1,13 +1,12 @@
+//import sun.security.util.Password;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class resetPassword extends JFrame {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
+    private JPanel resetPasswordPanel;
     private JLabel oldPassword;
     private JLabel newPassword;
     private JLabel confirmPassword;
@@ -15,33 +14,43 @@ public class resetPassword extends JFrame {
     private UserDatabaseManagement userDatabaseManagement = UserDatabaseManagement.getInstance();
 
     private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JPasswordField passwordField3;
 
     public resetPassword(User user) {
-
-        setTitle("Reset Password");
-        setSize(new Dimension(400, 400));
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setContentPane(panel1);
-        setLocationRelativeTo(null);
-        setVisible(true);
+        JDialog dialog = new JDialog((Frame) null, "Reset Password", true);//to make window always on top
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.setSize(400, 400);
+        dialog.setLocationRelativeTo(null);
+        dialog.setAlwaysOnTop(true);
+        dialog.add(resetPasswordPanel);
 
         Confirmbutton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String oldPassword = textField1.getText();
-                String newPassword = textField2.getText();
-                String confirmPassword = textField3.getText();
+                String oldPassword = passwordField1.getText();
+                String newPassword = passwordField2.getText();
+                String confirmPassword = passwordField3.getText();
+                String orignalPassword = user.getHashingPassword();
+                checkValid checkPassword = new checkValid(new TestPassword());
+                boolean passwordCheck =checkPassword.test(newPassword);
+                String CheckPasswordHashed = passwordHashing.hashpassword(oldPassword);
                 if (oldPassword.isEmpty() || newPassword.isEmpty() || confirmPassword.isEmpty()) {
-                    JOptionPane.showMessageDialog(null, "All fields are required", "Error", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(dialog, "All fields are required", "Error", JOptionPane.ERROR_MESSAGE);
+                } else if (!orignalPassword.equals(CheckPasswordHashed)){
+                    JOptionPane.showMessageDialog(dialog, "Wrong password", "Error", JOptionPane.ERROR_MESSAGE);
                 } else if (!(newPassword.equals(confirmPassword))) {
-                    JOptionPane.showMessageDialog(null, "Passwords do not match", "Error", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(dialog, "Passwords do not match", "Error", JOptionPane.ERROR_MESSAGE);
+                } else if (!passwordCheck) {
+                    JOptionPane.showMessageDialog(dialog, "Invalid Password", "Error", JOptionPane.ERROR_MESSAGE);
                 } else {
                     user.setPassword(newPassword);
-                    JOptionPane.showMessageDialog(null, "Password has been reset", "Success", JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.showMessageDialog(dialog, "Password has been reset", "Success", JOptionPane.INFORMATION_MESSAGE);
                     userDatabaseManagement.saveToFile();
-                    setVisible(false);
+                    dialog.dispose();
                 }
             }
         });
+        dialog.setVisible(true);
     }
 }
