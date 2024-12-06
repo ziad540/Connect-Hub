31539bc01435df4ba516090f7afc566514e64c11
ziad emad <eziad8002@gmail.com>
2024-12-06 22:56:54 +0200

    ziad modifiy

--- src/AddStory.java
+++
@@ -1,6 +0,0 @@
-//import java.util.ArrayList;
-//
-//public interface AddStory {
-//
-//    void addStory(ArrayList<Story> stories, Story s);
-//}
--- src/StoryAdd.java
+++
@@ -1,9 +0,0 @@
-//import java.util.ArrayList;
-//
-//public class StoryAdd implements AddStory{
-//    @Override
-//    public void addStory(ArrayList<Story> stories, Story s) {
-//        stories.add(s);
-//
-//    }
-//}
